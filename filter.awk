4889f5bc055086d09cf42134a52bc93a78cf7b19
Sebastien BERIDOT <sebastien.beridot@aviation-civile.gouv.fr>
2014-12-16 16:54:14 +0100

    minor correction "***" replaced with "999"

--- filter.awk
+++ filter.awk
@@ -1,6 +1,10 @@
 
 # input: filetype is BALISEP
 # output: lines with beacons and levels only
+# replace "***"  with "999"
 BEGIN { getline }
 
-/^3[ 12][A-Z0-9]{2,5} .*$/ { print }
+/^3[ 12][A-Z0-9]{2,5} .*$/ { 
+	sub(/\*\*\*/, "999", $0)
+	print
+}
--- gen.sh
+++ gen.sh
@@ -7,13 +7,18 @@
 #TMP="/tmp/tbtniv.$$.tmp"
 TMP="/tmp/tbtniv.$(date '+%d%b%Y-%kh%M').tmp"
 
+export PRETTY_FILE
+export PRETTY_SORT
+
+PRETTY_FILE=${BALISEP_FILE_ALPHA:-BALISEP_ALPHA.txt}
+PRETTY_SORT="TBTNIV > Bal."
 awk -f filter.awk | 
  awk -f extract.awk |
- awk -f process.awk > $TMP
-
-export PRETTY_FILE=${BALISEP_FILE_ALPHA:-BALISEP_ALPHA.txt}
-export PRETTY_SORT="TBTNIV > Bal."
-cat $TMP | sort -k2,2n -k3,3 -k1,1 | cut -d' ' -f 1,3-5 | awk -f pretty.awk > $PRETTY_FILE
+ awk -f process.awk |
+ tee $TMP |
+ sort -k2,2n -k3,3 -k1,1 |
+ cut -d' ' -f 1,3-5 |
+ awk -f pretty.awk > $PRETTY_FILE
 
 PRETTY_FILE=${BALISEP_FILE_NUMBER:-BALISEP_NUMBER.txt}
 PRETTY_SORT="Nb. > TBTNIV > Bal."
