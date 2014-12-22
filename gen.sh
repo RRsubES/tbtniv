a78f7bc0dd7021a826cbcd3a7ec1daaa9b63b246
sebastien <sberidot@libertysurf.fr>
2014-12-22 17:19:58 +0100

    usage function updated for tag option

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function usage {
-	echo ">> usage: ./$(basename $0) [-c|--checkfile] < BALISEP_FILE" >&2
+	echo ">> usage: ./$(basename $0) [-c|--checkfile] [-t|--tag TAG] < BALISEP_FILE" >&2
 	exit 1
 
 }
