d0ba910043b6eb6f0f656c5c76785ab8d8eaff1d
sebastien <sberidot@libertysurf.fr>
2014-12-23 01:19:07 +0100

    overwrite stuff erased, no need for that anymore minor typo fix

--- gen.sh
+++ gen.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 function usage {
-	echo ">> usage: ./$(basename $0) [-c|--checkfile] [-t|--tag TAG] < BALISEP_FILE" >&2
+	echo ">> usage: ./$(basename $0) [-t|--tag TAG] < BALISEP_FILE" >&2
 	exit 1
-
 }
 
 # checks if there is sthg redirected
@@ -13,13 +12,9 @@ if [ -p /dev/stdin -o -t 0 ]; then
 	usage
 fi
 
-OVERWRITE=1
 TAG=
 while (( $# > 0 )); do
 	case $1 in
-		-c|--checkfile)
-			shift
-			OVERWRITE=0;;
 		-t|--tag)
 			shift
 			TAG=_${1:-notag}
@@ -37,20 +32,12 @@ export DATE_CA
 export PRETTY_FILE
 export PRETTY_SORT
 
-function check_dst_file {
-	if [ $OVERWRITE -eq 0 -a -f $PRETTY_FILE ]; then
-		echo ">> $PRETTY_FILE existe, abandon" >&2
-		exit 2
-	fi
-}
-
 read HEADER
 DATE_CA=$(echo $HEADER | awk '{print $7}')
 echo ">> date CA: ${DATE_CA}" >&2
 
 PRETTY_FILE=BALISEP${TAG}_TB_${DATE_CA}.txt
 PRETTY_SORT="Tbtniv > Bal."
-check_dst_file 
 grep '^3[ 12][A-Z0-9]\{2,5\} .*$' |
  awk -f extract.awk |
  awk -f process.awk |
@@ -61,5 +48,8 @@ grep '^3[ 12][A-Z0-9]\{2,5\} .*$' |
 
 PRETTY_FILE=BALISEP${TAG}_NTB_${DATE_CA}.txt
 PRETTY_SORT="Nb. > Tbtniv > Bal."
-check_dst_file
-cat $TMP | sort -k4,4n -k2,2n -k3,3 -k1,1 | cut -d' ' -f 1,3-5 | awk -f pretty.awk > $PRETTY_FILE
+cat $TMP |
+ sort -k4,4n -k2,2n -k3,3 -k1,1 |
+ cut -d' ' -f 1,3-5 |
+ awk -f pretty.awk > $PRETTY_FILE
+
