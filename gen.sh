c9305ffa8f1d82db7aefd94f10c95a1f0b4e7a0a
sebastien <sberidot@libertysurf.fr>
2014-12-24 11:58:33 +0100

    TAG moved, easier to tab files in bash, avoids to complete by hand as much as before...

--- gen.sh
+++ gen.sh
@@ -41,7 +41,7 @@ fi
 DATE_CA=$(echo $HEADER | awk '{print $7}')
 echo ">> date CA: ${DATE_CA}" >&2
 
-PRETTY_FILE=BALISEP${TAG}_TB_${DATE_CA}.txt
+PRETTY_FILE=BALISEP_TB_${DATE_CA}${TAG}.txt
 PRETTY_SORT="Tbtniv > Bal."
 grep '^3[ 12][A-Z0-9]\{2,5\} .*$' |
  awk -f extract.awk |
@@ -51,7 +51,7 @@ grep '^3[ 12][A-Z0-9]\{2,5\} .*$' |
  cut -d' ' -f 1,3-5 |
  awk -f pretty.awk > $PRETTY_FILE
 
-PRETTY_FILE=BALISEP${TAG}_NTB_${DATE_CA}.txt
+PRETTY_FILE=BALISEP_NTB_${DATE_CA}${TAG}.txt
 PRETTY_SORT="Nb. > Tbtniv > Bal."
 cat $TMP |
  sort -k4,4n -k2,2n -k3,3 -k1,1 |
