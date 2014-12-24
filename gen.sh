59d36b0a9c4e8234a99a09a3e895b7a3767d43fe
sebastien <sberidot@libertysurf.fr>
2014-12-24 15:13:20 +0100

    DATE_DELIVER added and extracted from the source file, passed to awk scrips new option --extrainfo | -e to display them fix for header regex minor comment correction

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function usage {
-	echo ">> usage: ./$(basename $0) [-t|--tag TAG] < BALISEP_FILE" >&2
+	echo ">> usage: ./$(basename $0) [-e|--extrainfo] [-t|--tag TAG] < BALISEP_FILE" >&2
 	exit 1
 }
 
@@ -12,12 +12,22 @@ if [ -t 0 ]; then
 	usage
 fi
 
+export DATE_CA
+export DATE_DELIVER
+export PRETTY_FILE
+export PRETTY_SORT
+export PRETTY_EXTRAINFO
+
 TAG=
+PRETTY_EXTRAINFO=0
 while (( $# > 0 )); do
 	case $1 in
 		-t|--tag)
 			shift
 			TAG=_${1:-notag}
+			shift;;
+		-e|--extrainfo)
+			PRETTY_EXTRAINFO=1
 			shift;;
 		-h|--help|*)
 			usage;;
@@ -28,18 +38,16 @@ done
 #TMP="/tmp/tbtniv.$$.tmp"
 TMP="/tmp/tbtniv.$(date '+%0d%0b%Y-%0kh%0M').tmp"
 
-export DATE_CA
-export PRETTY_FILE
-export PRETTY_SORT
-
 read HEADER
-{ echo $HEADER | grep '^FORMAT : STIP VERSION CA : [ 0-9]\{1,2\}-[ 0-9]\{1,2\}-[0-9]\{2\} LIVRAISON : [ 0-9]\{1,2\}-[ 0-9]\{1,2\}-[0-9]\{2\} PART : BALISEP .*$'; } > /dev/null
+{ echo $HEADER | grep '^FORMAT : STIP [ ]*VERSION CA : [ 0-9]\{1,2\}-[ 0-9]\{1,2\}-[0-9]\{2\} [ ]*LIVRAISON : [ 0-9]\{1,2\}-[ 0-9]\{1,2\}-[0-9]\{2\} [ ]*PART : BALISEP.*$'; } > /dev/null
 if [ $? -ne 0 ]; then
 	echo ">> entête de fichier non valide" >&2
 	exit 3
 fi
 DATE_CA=$(echo $HEADER | awk '{print $7}')
+DATE_DELIVER=$(echo $HEADER | awk '{print $10 }')
 echo ">> date CA: ${DATE_CA}" >&2
+echo ">> date Livraison: ${DATE_DELIVER}" >&2
 
 PRETTY_FILE=BALISEP_TB_${DATE_CA}${TAG}.txt
 PRETTY_SORT="Tbtniv > Bal."
--- pretty.awk
+++ pretty.awk
@@ -2,7 +2,7 @@
 # output:
 function mult(c, n)
 {
-	# n must be positive
+	# n > 0 
 	l = 1
 	s = c
 	while (2*l <= n) {
@@ -16,8 +16,12 @@ function mult(c, n)
 }
 
 BEGIN {
-	printf("TBTNIV au %s\nClassement [%s]\n\n", ENVIRON["DATE_CA"], ENVIRON["PRETTY_SORT"])
-	printf("%-5s %-39s %3s %3s\n%s\n", "Bal.", "Tbtniv", "Nb.", "Tot", mult("-", 53)) 
+	if (ENVIRON["PRETTY_EXTRAINFO"] == 1)
+		printf("Date CA du %s\nLivraison le %s\nClassement [%s]\n\n",
+			ENVIRON["DATE_CA"], ENVIRON["DATE_DELIVER"], ENVIRON["PRETTY_SORT"])
+	printf("%-5s %-39s %3s %3s\n", "Bal.", "Tbtniv", "Nb.", "Tot") 
+	if (ENVIRON["PRETTY_EXTRAINFO"] == 1)
+		printf("%s\n", mult("-", 53)) 
 }
 
 {
