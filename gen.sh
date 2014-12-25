75dec80cc520933c66115420576b1273f3542726
sebastien <sberidot@libertysurf.fr>
2014-12-25 01:20:41 +0100

    first simple version no as many options, only remains tag and noheader in BALISEP

--- gen.sh
+++ gen.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 function usage {
-	echo ">> usage: ./$(basename $0) [-e|--extrainfo] [-t|--tag TAG] < BALISEP_FILE" >&2
-	echo ">> e.g.: ./$(basename $0) --extrainfo --tag IBP < BALISEP"
-	echo ">> e.g.: ./$(basename $0) -t \\\${DATE_DELIVER} < BALISEP"
+	echo ">> usage: ./$(basename $0) [-s|--skipheader] [-t|--tag TAG] < BALISEP_FILE" >&2
 	exit 1
 }
 
@@ -18,18 +16,17 @@ export DATE_CA
 export DATE_DELIVER
 export PRETTY_FILE
 export PRETTY_SORT
-export PRETTY_EXTRAINFO
 
 TAG=
-PRETTY_EXTRAINFO=0
+NO_HEADER=0
 while (( $# > 0 )); do
 	case $1 in
 		-t|--tag)
 			shift
 			TAG=_${1:-notag}
 			shift;;
-		-e|--extrainfo)
-			PRETTY_EXTRAINFO=1
+		-n|--noheader)
+			NO_HEADER=1
 			shift;;
 		-h|--help|*)
 			usage;;
@@ -40,18 +37,22 @@ done
 #TMP="/tmp/tbtniv.$$.tmp"
 TMP="/tmp/tbtniv.$(date '+%0d%0b%Y-%0kh%0M').tmp"
 
-read HEADER
-{ echo $HEADER | grep '^FORMAT : STIP [ ]*VERSION CA : [ 0-9]\{1,2\}-[ 0-9]\{1,2\}-[0-9]\{2\} [ ]*LIVRAISON : [ 0-9]\{1,2\}-[ 0-9]\{1,2\}-[0-9]\{2\} [ ]*PART : BALISEP.*$'; } > /dev/null
-if [ $? -ne 0 ]; then
-	echo ">> [ERROR]: entête de fichier non valide" >&2
-	exit 3
+DATE_CA=
+DATE_DELIVER=
+if [ $NO_HEADER -eq 0 ]; then
+	read HEADER
+	{ echo $HEADER | grep '^FORMAT : STIP [ ]*VERSION CA : [ 0-9]\{1,2\}-[ 0-9]\{1,2\}-[0-9]\{2\} [ ]*LIVRAISON : [ 0-9]\{1,2\}-[ 0-9]\{1,2\}-[0-9]\{2\} [ ]*PART : BALISEP.*$'; } > /dev/null
+	if [ $? -ne 0 ]; then
+		echo ">> [ERROR]: entête de fichier non valide" >&2
+		exit 3
+	fi
+	DATE_CA=_$(echo $HEADER | awk '{print $7}')
+	DATE_DELIVER=$(echo $HEADER | awk '{print $10}')
+	echo ">> date CA: ${DATE_CA:1}" >&2
+	echo ">> date Livraison: ${DATE_DELIVER}" >&2
 fi
-DATE_CA=$(echo $HEADER | awk '{print $7}')
-DATE_DELIVER=$(echo $HEADER | awk '{print $10 }')
-echo ">> date CA: ${DATE_CA}" >&2
-echo ">> date Livraison: ${DATE_DELIVER}" >&2
 
-PRETTY_FILE=BALISEP_TB_${DATE_CA}$(eval echo ${TAG}).txt
+PRETTY_FILE=BALISEP_TB${DATE_CA}$(eval echo ${TAG}).txt
 PRETTY_SORT="Tbtniv > Bal."
 grep '^3[ 12][A-Z0-9]\{2,5\} .*$' |
  awk -f extract.awk |
@@ -61,7 +62,7 @@ grep '^3[ 12][A-Z0-9]\{2,5\} .*$' |
  cut -d' ' -f 1,3-5 |
  awk -f pretty.awk > $PRETTY_FILE
 
-PRETTY_FILE=BALISEP_NTB_${DATE_CA}$(eval echo ${TAG}).txt
+PRETTY_FILE=BALISEP_NTB${DATE_CA}$(eval echo ${TAG}).txt
 PRETTY_SORT="Nb. > Tbtniv > Bal."
 cat $TMP |
  sort -k4,4n -k2,2n -k3,3 -k1,1 |
--- pretty.awk
+++ pretty.awk
@@ -1,27 +1,7 @@
 # input: (sorted) BEACON TBTNIV NUMBER_OF_BEACONS_USING_THIS_TBTNIV TOTAL_NUMBER_OF_DIFFERENT_TBTNIV
 # output:
-function mult(c, n)
-{
-	# n > 0 
-	l = 1
-	s = c
-	while (2*l <= n) {
-		s = s "" s
-		l*=2
-	}
-	n-=l
-	if (n > 0)
-		s = s "" substr(s, 0, n)
-	return s
-}
-
 BEGIN {
-	if (ENVIRON["PRETTY_EXTRAINFO"] == 1)
-		printf("Date CA du %s\nLivraison le %s\nClassement [%s]\n\n",
-			ENVIRON["DATE_CA"], ENVIRON["DATE_DELIVER"], ENVIRON["PRETTY_SORT"])
 	printf("%-5s %-39s %3s %3s\n", "Bal.", "Tbtniv", "Nb.", "Tot") 
-	if (ENVIRON["PRETTY_EXTRAINFO"] == 1)
-		printf("%s\n", mult("-", 53)) 
 }
 
 {
