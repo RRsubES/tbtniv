8a5689c085ebc33124e3c6bbcdd4e15d10a38882
sebastien <sberidot@libertysurf.fr>
2015-01-15 14:36:38 +0100

    minor fix for $TMP filename

--- gen.sh
+++ gen.sh
@@ -56,8 +56,6 @@ if [ -t 0 ]; then
 	usage
 fi
 
-#TMP=$(mktemp tbtniv.XXXX.tmp)
-#TMP="/tmp/tbtniv.$$.tmp"
 TMP="/tmp/tbtniv.$(date '+%0d%0b%Y-%0kh%0M').tmp"
 
 # normal process:
@@ -82,14 +80,14 @@ sed 's/\r//g' |
 #grep '^3[ 12][A-Z0-9]\{2,5\}.*$' |
  awk -f extract.awk |
  awk -f process.awk |
- tee $TMP |
+ tee "$TMP" |
  sort -k2,2n -k3,3 -k1,1 |
  cut -d' ' -f 1,3-5 |
  awk -f pretty.awk > "$PRETTY_FILE"
 
 PRETTY_FILE="BALISEP_NTB_${DATE_CA}$(eval echo ${TAG}).txt"
 PRETTY_SORT="Nb. > Tbtniv > Bal."
-cat $TMP |
+cat "$TMP" |
  sort -k4,4n -k2,2n -k3,3 -k1,1 |
  cut -d' ' -f 1,3-5 |
  awk -f pretty.awk > "$PRETTY_FILE"
