f9c65b53d8886eafac6f582f637f0eefaff24588
sebastien <sberidot@libertysurf.fr>
2015-01-15 23:21:11 +0100

    count.sh now fixed, forgot to initialize correctly the prev var

--- count.sh
+++ count.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/awk -f
+# ./check.sh < BALISEP.jan15 | sort | uniq | wc -l
 
 BEGIN {
 	getline
+	GROUND = "000"
+	prev = GROUND
 }
 
-$2 != prev {
-	prev = $2
-	count++
+/^1 [A-Z0-9]{2,5} .*$/ {
+	if (prev != GROUND)
+		print prev
+	prev = GROUND
+	next
 }
+
+/^3[ 12][A-Z0-9]{2,5} .*$/ {
+	sub(/\*\*\*/, "999", $0)
+#print $0
+	if (substr($0, 2, 1) == " ") 
+		start = 3
+	else
+		start = 2
+	for(i=start; i < NF; i+=2)
+		prev = prev "-" $i
+}
+
 END {
-	printf("nb tbtniv: %d\n", count)
+	if (prev != GROUND) 
+		print prev
+
 }
