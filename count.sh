75bb906c66b056e7c02dd6ebce991fa1000faaa2
sebastien <sberidot@libertysurf.fr>
2015-01-16 11:40:16 +0100

    piping some process in count.sh awk scripts sorting as well to allow easy stats

--- count.sh
+++ count.sh
@@ -3,6 +3,13 @@
 # ./count.sh < BALISEP.jan15 | sort | uniq | wc -l
 # to show stats about tbtniv, sorted...
 # ./count.sh < BALISEP.jan15 | sort -k2,2n | uniq -c | sort -k1,1n
+function lvl(t) {
+	return (length(t) + 1) / 4
+}
+
+function echo(p) {
+	printf("%3d %s\n", lvl(p), p) | "sort -k1,1n -k2,2 | uniq -c"
+}
 
 BEGIN {
 	#ignore first line
@@ -13,7 +20,7 @@ BEGIN {
 
 /^1 [A-Z0-9]{2,5} .*$/ {
 	if (prev != GROUND) {
-		print prev
+		echo(prev)
 		prev = GROUND
 	}
 	next
@@ -30,5 +37,5 @@ BEGIN {
 }
 
 END {
-	print prev
+	echo(prev)
 }
