8e069e88cf7dd60e0ec94f6e6711b33772c761b5
sebastien <sberidot@libertysurf.fr>
2015-02-22 23:08:46 +0100

    the generated directory is now correclty managed by clear... script file.

--- clear.gen.sh
+++ clear.gen.sh
@@ -3,6 +3,6 @@ IFS=$'\n'; for f in `find *.txt ! -type l`; do
 	rm "$f"
 done
 
-IFS=$'\n'; for d in `find . -maxdepth 1 -type d | grep '^./[0-9]\{1,5\}\.[0-9]\{2\}.*\.[0-9]\{4\}-[0-9]\{2\}h[0-9]\{2\}'`; do
+IFS=$'\n'; for d in `find . -maxdepth 1 -type d | grep '^./[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}_[0-9]\{2\}h[0-9]\{2\}_CA[0-9]\{2\}-[0-9]\{2\}-[0-9]\{2\}'`; do
 	rm -Rf "$d"
 done
